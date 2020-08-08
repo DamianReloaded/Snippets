d3e13c7d46069cf80ab94ebaa5e1ca39f35188ae
DamianReloaded <damianpaz@ymail.com>
2020-08-08 17:26:16 -0300

    Update main.cpp

--- list_comprehension/main.cpp
+++ list_comprehension/main.cpp
@@ -6,82 +6,82 @@
 #include <list>
 #include <string>
 
-// Mimic the handyness of list comprehension
-// result = [x*x for x in nums]
-
 template <typename T, size_t S>
 class into : public std::array<T,S>
 {
   public:
-    into (std::array<T,S>& input, auto func) 
+    into (std::array<T,S>& input, auto func)
     {
       for (size_t i=0; i<this->size(); i++) (*this)[i] = func(input[i]);
     }
-    std::array<T,S> operator ()() { return *this; }
+
 };
 
 template<typename T, template<typename...> class C, typename... Args>
-class in 
+class in
 {
   public:
-    in (C<T, Args...>& input, auto func) 
+    in (C<T, Args...>& input, auto func)
     {
       for (auto inp=input.begin(); inp!=input.end(); inp++) (void)items.insert(items.end(),func(*inp));
     }
-    const auto&   operator [] (const size_t& i) { auto iter = items.begin(); std::advance(iter,i); return *iter; }
-    C<T, Args...> operator () ()                { return items; }
+
+    auto                    begin  () const                { return items.begin(); }
+    auto                    end    () const                { return items.end(); }
+    const auto& operator    []     (const size_t& i) const { auto iter = items.begin(); std::advance(iter,i); return *iter; }
+    operator                auto   () const                { return items; }
+
   private:
     C<T, Args...> items;
 };
 
-#define each(x) [](auto x) 
-#define is(x) {return x;}
+#define each(x,f) [](auto x) {return f;}
 
 using namespace std;
 int main()
 {
-  switch(7)
+  switch(6)
   {
     case 1:
     {
       array nums = {1,2,3,4};
-      auto result = into(nums, each (x) is(x*x)); 
+      auto result = into(nums, each (x, x*x)); 
       return result[1];
     }   
     case 2:
     {
       string letters = "hello";
-      auto result = in(letters, each(x) is(toupper(x)) );
+      auto result = in(letters, each(x, toupper(x)) );
       return result[1];
     }
     case 3:
     {
       list nums = {1,2,3,4};
-      auto result = in(nums, each(x) is((true)?x*x:x) );
+      auto result = in(nums, each(x, (true)?x*x:x) );
       return result[1];
     }
     case 4:
     {
       set nums = {1,2,3,4};
-      auto result = in(nums, each(x) is(x*x) );
+      auto result = in(nums, each(x, x*x) );
       return result[1];
     }
     case 5:
     {
       deque nums = {1,2,3,4};
-      auto result = in(nums, each(x) is(x*x) );
+      auto result = in(nums, each(x, x*x) );
       return result[1];
     }
     case 6:
     {
       vector nums = {1,2,3,4};
-      auto result = in(nums, each(x) is((true)?x*x:x) );
+      auto result = in(nums, each(x, (true)?x*x:x) );
       return result[1];
     }    
     case 7:
     {
       map<int,string> nums = { {1,"one"}, {2,"two"}, {3,"three"}, {4,"four"} }; 
-      auto result = in(nums, each(x) is(make_pair(x.first*x.first,x.second)) );
+      auto result = in(nums, each(x, make_pair(x.first*x.first,x.second)) );
       return result[1].first;
     }
   }
