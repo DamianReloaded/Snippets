7700dfb79aac45d45961c817d1d56543672a8b22
Damian Paz <dpaz@itrio.net>
2024-05-11 08:46:17 -0300

    modified pool main

--- pool/main.cpp
+++ pool/main.cpp
@@ -1,10 +1,4 @@
-//#include "source/SimpleHtmlServer/simple_html_server.h"
-//#include "source/QLearning/qlearning01.h"
-//#include "source/SimpleWebsockets/simple_websocket_server.h"
-//#include <chrono>
-//#include <thread>
-#include "ecs/example_ecs.h"
-#include "ecs/example_pool.h"
+#include "example_pool.h"
 
 static inline std::chrono::time_point<std::chrono::steady_clock> getCurrentTimeMillis() {
     return std::chrono::steady_clock::now();
@@ -12,7 +6,6 @@ static inline std::chrono::time_point<std::chrono::steady_clock> getCurrentTimeM
 
 int main()
 {
-//    ecs::example_ecs();
     ecs::example_pool();
     ecs::example_no_pool();
     return 0;
